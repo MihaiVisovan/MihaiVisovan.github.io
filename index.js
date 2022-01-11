e7714c65e415783056354f92fe36c5d9da6d98bd
Mihai <mihai_visovan@yahoo.com>
2022-01-11 17:03:52 +0200

    Fixed responsiveness

--- index.js
+++ index.js
@@ -26,3 +26,35 @@ const showProjects = () => {
     menu.classList.remove('text-pop-up-bottom');
   }, 1000);
 };
+
+window.addEventListener('DOMContentLoaded', () => {
+  adjustDimensions();
+  window.addEventListener('resize', () => {
+    adjustDimensions();
+  });
+});
+
+const adjustDimensions = () => {
+  const firstCurve = document.querySelector('#first-curve');
+  const secondCurve = document.querySelector('#second-curve');
+  const svgContainer = document.querySelector('#svg-container');
+  const windowWidth = window.innerWidth;
+  if (windowWidth <= 767) {
+    svgContainer.setAttribute('height', 400);
+    svgContainer.setAttribute('width', 400);
+    firstCurve.setAttribute('d', 'M100 200, A1 1, 0, 0 1, 300 200');
+    secondCurve.setAttribute('d', 'M80 210, A1 1, 0, 0 0, 320 210');
+    return;
+  }
+  if (windowWidth <= 1700) {
+    svgContainer.setAttribute('height', 600);
+    svgContainer.setAttribute('width', 600);
+    firstCurve.setAttribute('d', 'M100 300, A1 1, 0, 0 1, 500 300');
+    secondCurve.setAttribute('d', 'M60 310, A1 1, 0, 0 0, 540 310');
+    return;
+  }
+  svgContainer.setAttribute('height', 800);
+  svgContainer.setAttribute('width', 800);
+  firstCurve.setAttribute('d', 'M100 400, A1 1, 0, 0 1, 700 400');
+  secondCurve.setAttribute('d', 'M50 410 A1 1, 0, 0 0, 750 410');
+};
